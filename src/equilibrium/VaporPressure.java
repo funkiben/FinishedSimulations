789ac1b5c57a114f24f990b107b2f20791acbf01
JTisboss <jdt103@comcast.net>
2017-02-07 09:56:21 -0500

    Set Up Some Components
    Just me messing around with some buttons and a graph.

--- src/equilibrium/VaporPressure.java
+++ src/equilibrium/VaporPressure.java
@@ -1,25 +1,66 @@
 package equilibrium;
 
 import lab.LabFrame;
-import lab.component.input.ButtonComponent;
+import lab.component.HorizontalGraduation;
+import lab.component.VerticalGraduation;
+import lab.component.data.DataTable;
+import lab.component.data.Graph;
+import lab.component.swing.input.ButtonComponent;
+import lab.component.swing.input.CheckBoxComponent;
 
 //Vapor pressure simulation lab from Ms. Lund Easy Java
 
 public class VaporPressure extends LabFrame{
+	private static final long serialVersionUID = 1L;
 	//initial values from simulation
 	int temperature = 0;
 	int dtemperature = 1;
 	double volume = 1;
-	double R = 8.313;
+	double R = 8.314;
 	int time = 0;
 	int dtime = 1;
 	
 	ButtonComponent play;
 	ButtonComponent step;
 	ButtonComponent reset;
+	Graph molarity;
+	Graph vaporPressure;
+	Graph pressure;
+	CheckBoxComponent showTank;
+	CheckBoxComponent showEquipment;
+	CheckBoxComponent showPressureGraph;
+	DataTable<Double> vaporPressureMolarity;
+	DataTable<Double> vaporPressureTime;
+	
+	public static void main(String args[]){
+		new VaporPressure("Vapor Pressure Lab", 700, 800);
+	}
+	
 	public VaporPressure(String name, int width, int height) {
-		super("Vapor Pressure Lab", 700, 800);
-
+		super(name, width, height);
+		play = new ButtonComponent(100, 25, "Play"){
+			@Override
+			public void doSomething() {
+				
+			}
+		};
+		step = new ButtonComponent(100, 25, "Step"){
+			@Override
+			public void doSomething() {
+				
+			}
+		};
+		reset = new ButtonComponent(100, 25, "Reset"){
+			@Override
+			public void doSomething() {
+				
+			}
+		};
+		HorizontalGraduation horizontalGraduation = new HorizontalGraduation(0, 100, 20, 10);
+		VerticalGraduation verticalGraduation = new VerticalGraduation(54, 55.8, .2, .1);
+		molarity = new Graph(750, 500, "Molarity vs Time 20C, 30C, 40C, 60C", "Molarity H2O (mol/L)", "Time (s)", verticalGraduation, horizontalGraduation);
+		addComponent(molarity, play, step, reset);
+		start(30);
 	}
 
 	@Override
