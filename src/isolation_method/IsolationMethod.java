d62be650f071d5a06198b9981f6838d688694a72
funkitech <funkiben@gmail.com>
2017-03-28 09:55:22 -0400

    Worked on IsolationMethod graphs

--- src/isolation_method/IsolationMethod.java
+++ src/isolation_method/IsolationMethod.java
@@ -4,10 +4,14 @@ import java.awt.Color;
 
 import lab.LabFrame;
 import lab.component.EmptyComponent;
+import lab.component.data.Graph;
+import lab.component.data.GraphDataSet;
 import lab.component.geo.Rectangle;
 import lab.component.swing.Label;
 import lab.component.swing.input.Button;
 import lab.component.swing.input.field.DoubleField;
+import lab.util.HorizontalGraduation;
+import lab.util.VerticalGraduation;
 
 public class IsolationMethod extends LabFrame {
 
@@ -18,17 +22,17 @@ public class IsolationMethod extends LabFrame {
 	}
 	
 	private final ReactionApparatus reactionApparatus;
-	private final Button start;
-	private final Button stop;
-	private final Button reset;
-	private final DoubleField O2Amount;
-	private final DoubleField NOAmount;
+	private final Button start, stop, reset;
+	private final DoubleField O2Amount, NOAmount;
+	
+	private final Graph zeroOrderGraph, firstOrderGraph, secondOrderGraph;
+	private final GraphDataSet zeroOrderData, firstOrderData, secondOrderData;
 	
 	public IsolationMethod() {
-		super("Isolation Method", 800, 650);
+		super("Isolation Method", 1000, 650);
 		
 		reactionApparatus = new ReactionApparatus();
-		reactionApparatus.setOffset(30, 30);
+		reactionApparatus.setOffset(15, 30);
 		addComponent(reactionApparatus);
 		
 		
@@ -69,6 +73,48 @@ public class IsolationMethod extends LabFrame {
 		controlArea.setOffsetY(380);
 		
 		reactionApparatus.addChild(controlArea);
+		
+		
+		HorizontalGraduation hg;
+		VerticalGraduation vg;
+		
+		
+		hg = new HorizontalGraduation(0, 1, 1, 0.1);
+		vg = new VerticalGraduation(0, 1, 1, 0.1);
+
+		hg.setShowLabels(false);
+		
+		zeroOrderGraph = new Graph(200, 200, "Zero Order", "t (s)", "C", hg, vg);
+		zeroOrderGraph.setOffsetX(50);
+		addComponent(zeroOrderGraph);
+		
+		zeroOrderData = new GraphDataSet("NO2", false, false);
+		zeroOrderGraph.addDataSet(zeroOrderData);
+		
+		hg = new HorizontalGraduation(0, 1, 1, 0.1);
+		vg = new VerticalGraduation(0, 1, 1, 0.1);
+		
+		hg.setShowLabels(false);
+		
+		firstOrderGraph  = new Graph(200, 200, "First Order", "t (s)", "ln(Cf-C)", hg, vg);
+		firstOrderGraph.setOffsetX(50);
+		addComponent(firstOrderGraph);
+		
+		firstOrderData = new GraphDataSet("NO2", false, false);
+		firstOrderGraph.addDataSet(firstOrderData);
+		
+		hg = new HorizontalGraduation(0, 1, 1, 0.1);
+		vg = new VerticalGraduation(0, 1, 1, 0.1);
+		
+		hg.setShowLabels(false);
+		
+		secondOrderGraph  = new Graph(200, 200, "Second Order", "t (s)", "1/(Cf-C)", hg, vg);
+		secondOrderGraph.setOffsetX(50);
+		addComponent(secondOrderGraph);
+		
+		secondOrderData = new GraphDataSet("NO2", false, false);
+		secondOrderGraph.addDataSet(secondOrderData);
+		
 		
 		start(30);
 		
